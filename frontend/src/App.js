f3d91effef661636c78b1748175c825b7f19691a
Marcio L Lesniovski <marcio.lesni@gmail.com>
2020-09-19 22:32:49 -0300

    [Marcio][TSK033]Cria projeto fronted

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import Header from './Header';
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  function increment(){
+    setCounter(counter +1);
+  }
+
+  return (
+    <div><Header title="Travel System">
+      Contador: {counter} </Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
+  );
+}
+
+export default App;
