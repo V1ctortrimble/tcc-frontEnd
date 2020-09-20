9141dd0cfa7fe583a453f7c41709ba8d8d1e5579
Marcio L Lesniovski <marcio.lesni@gmail.com>
2020-09-20 00:06:30 -0300

    [Marcio][TSK034]Cria Pagina Login

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {FiKey} from 'react-icons/fi'
 
 import './styles.css';
 
+import travelLogoImg from '../../assets/TravelLogo.png';
+import travelImg from '../../assets/TravelsWorld.png';
+
 export default function Login(){
     return (
-        <h1>Hello World</h1>
+        <div className="login-container">
+            
+        <section className="form">
+            <img src ={travelLogoImg} alt="Logo Travel"/>
+
+            <form>
+                <h1>Faça seu login</h1>
+                <input placeholder="E-mail"/>
+                <input placeholder="Senha"/>
+                <button className="button" type="submit">Entrar</button>
+                <a href="/forgetPass">
+                    <FiKey size={16} color="#E02041"/>
+                    Recuperar a senha
+                </a>
+            </form>
+
+        </section>
+        <img src ={travelImg} alt="Imagem Viagens"/>
+        </div>
     )
 }
